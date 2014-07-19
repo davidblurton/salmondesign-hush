a85440c0cdf73b17e277c5de7c00238cc8f3d5df
David Blurton <david.blurton@gmail.com>
2014-07-19 18:07:25 +0100

    Only version assets in production

--- Brocfile.js
+++ Brocfile.js
@@ -1,16 +1,29 @@
 var jade = require('broccoli-jade');
 var mergeTrees = require('broccoli-merge-trees');
-var compileSass = require('broccoli-ruby-sass');
+var compileSass = require('broccoli-sass');
 var autoprefixer = require('broccoli-autoprefixer');
+var assetRev = require('broccoli-asset-rev');
+var env = require('broccoli-env').getEnv();
 
 var html = jade('templates', {
   basedir: 'templates'
 });
 
-var css = compileSass(['styles'], 'main.scss', 'style.css', {outputStyle: 'compressed'});
+var css = compileSass(['styles'], 'main.scss', 'style.css', {
+  outputStyle: 'compressed'
+});
 
 css = autoprefixer(css);
 
 var assets = 'public';
 
-module.exports = mergeTrees([html, css, assets]);
+var tree = mergeTrees([html, css, assets]);
+
+if (env === 'production') {
+  tree = assetRev(merged, {
+    extensions: ['js', 'css', 'png', 'jpg', 'gif'],
+    replaceExtensions: ['html', 'js', 'css'],
+  });
+}
+
+module.exports = tree;

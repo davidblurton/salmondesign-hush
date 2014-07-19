b0be6e0ebe0c8b618392ce05dce830eddab1587c
David Blurton <david.blurton@gmail.com>
2014-07-19 16:36:36 +0100

    Do desktop site

--- Brocfile.js
+++ Brocfile.js
@@ -1,13 +1,13 @@
 var jade = require('broccoli-jade');
 var mergeTrees = require('broccoli-merge-trees');
-var compileSass = require('broccoli-sass');
+var compileSass = require('broccoli-ruby-sass');
 var autoprefixer = require('broccoli-autoprefixer');
 
 var html = jade('templates', {
   basedir: 'templates'
 });
 
-var css = compileSass(['styles'], 'main.scss', 'style.css', {sourceMap: true, outputStyle: 'compressed'});
+var css = compileSass(['styles'], 'main.scss', 'style.css', {outputStyle: 'compressed'});
 
 css = autoprefixer(css);
 
